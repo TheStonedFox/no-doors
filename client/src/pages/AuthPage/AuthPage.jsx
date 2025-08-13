baa9beb5498ebc61d73d5936e470ec0bf4416bc4
TheStonedFox <lesha3772@gmail.com>
2025-08-14 02:48:59 +0300

    deploy

--- client/src/pages/AuthPage/AuthPage.jsx
+++ client/src/pages/AuthPage/AuthPage.jsx
@@ -12,7 +12,7 @@ import { login, register } from '@api/api'
 import { addNotification } from '../../redux/features/uiSlice'
 
 import { useDispatch } from 'react-redux'
-import RegisterAdvantagesIcon from '../../svgIcons/RegisterAdvantagesIcon'
+import RegisterAdvantagesIcon from '../../svgIcons/RegisterAdvantagesIcon.jsx'
 
 import { generateId } from '@utils/generateId'
 
